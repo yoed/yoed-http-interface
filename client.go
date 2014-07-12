6f371d329a1f2af4af6b6d3dab4233212ce8145e
Arnaud BRETON <arnaud@videonot.es>
2014-07-12 14:05:00 +0200

    Refactoring loading config

--- client.go
+++ client.go
@@ -5,6 +5,9 @@ import (
 	"net/url"
 	"log"
 	"fmt"
+	"os"
+	"encoding/json"
+	"io/ioutil"
 )
 
 type YoedClient interface {
@@ -23,6 +26,50 @@ type BaseYoedClient struct {
 }
 func (c *BaseYoedClient) GetConfig() (*BaseYoedClientConfig) {
 	return c.Config
+}
+func (c *BaseYoedClient) loadConfig(configPath string) (*BaseYoedClientConfig, error) {
+	configJson, err := ReadConfig(configPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config := &BaseYoedClientConfig{}
+
+	if err := json.Unmarshal(configJson, config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func NewBaseYoedClient() (*BaseYoedClient, error) {
+	c := &BaseYoedClient{}
+	config, err := c.loadConfig("./config.json")
+
+	if err != nil {
+		panic(fmt.Sprintf("failed loading config: %s", err))
+	}
+
+	c.Config = config
+
+	return c, nil
+}
+
+func ReadConfig(configPath string) ([]byte, error) {
+	configFile, err := os.Open(configPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	configJson, err := ioutil.ReadAll(configFile)
+
+	if err != nil {
+		return nil, err
+	} else {
+		return configJson, nil
+	}
 }
 
 func Run(c YoedClient) {
@@ -43,7 +90,7 @@ func Run(c YoedClient) {
 	resp, err := http.PostForm(config.ServerUrl+"/yo", url.Values{"handle":{config.Handle}, "callback_url":{"http://"+config.Listen}})
 
 	if err != nil {
-		panic(fmt.Sprintf("failed contacting server : %s", err))
+		panic(fmt.Sprintf("Failed contacting server : %s", err))
 	}
 	log.Printf("Yoed server answer... %s", resp)
 	log.Printf("Listening...")
